dd46393d8d27363cfcd16c0ff6f1cbb319366040
ishaan-gupta-au <89191839+ishaan-gupta-au@users.noreply.github.com>
2021-09-26 14:41:19 +1000

    Upload json and js files

---
+++ js/vega_lite_vis.js
@@ -0,0 +1,8 @@
+var vg_1 = "reef_map.json"
+
+vegaEmbed("#coral_map",vg_1).then(function(result){
+
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result view
+
+}).catch(console.error);
+
