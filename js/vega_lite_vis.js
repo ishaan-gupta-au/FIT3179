8ed4ae9a55f8879bdefa52b980a15f3279bb937e
ishaan-gupta-au <89191839+ishaan-gupta-au@users.noreply.github.com>
2021-10-09 10:23:27 +1100

    Upload vega lite and json files

--- js/vega_lite_vis.js
+++ js/vega_lite_vis.js
@@ -1,6 +1,6 @@
-var vg_1 = "reef_map.json"
+var vg_1 = "global_emissions.json"
 
-vegaEmbed("#coral_map",vg_1).then(function(result){
+vegaEmbed("#share_emissions",vg_1).then(function(result){
 
     // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result view
 
