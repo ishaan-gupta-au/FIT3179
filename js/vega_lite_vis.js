1010b9305c021ae2eb84f43667eca8875f2cac47
ishaan-gupta-au <89191839+ishaan-gupta-au@users.noreply.github.com>
2021-10-14 14:09:42 +1100

    Delete vega_lite_vis.js

--- js/vega_lite_vis.js
+++
@@ -1,8 +0,0 @@
-var vg_1 = "global_emissions.json"
-
-vegaEmbed("#share_emissions",vg_1).then(function(result){
-
-    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result view
-
-}).catch(console.error);
-
